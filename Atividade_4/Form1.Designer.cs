96b17b412ec1bf436785dfbcf19e734c6c238b49
PauloCervencove <62033278+PauloCervencove@users.noreply.github.com>
2020-06-08 07:55:36 -0300

    Add files via upload

---
+++ Atividade_4/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Atividade4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNomeFunc = new System.Windows.Forms.Label();
+            this.lblSalBruto = new System.Windows.Forms.Label();
+            this.lblNumFilhos = new System.Windows.Forms.Label();
+            this.lblDados = new System.Windows.Forms.Label();
+            this.lblAliquotaINSS = new System.Windows.Forms.Label();
+            this.lblAliquotaIRFF = new System.Windows.Forms.Label();
+            this.lblSalFamilia = new System.Windows.Forms.Label();
+            this.lblSalLiquido = new System.Windows.Forms.Label();
+            this.txtNomeFunc = new System.Windows.Forms.TextBox();
+            this.txtSalBruto = new System.Windows.Forms.TextBox();
+            this.txtNumFilhos = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.txtAliquotaINSS = new System.Windows.Forms.TextBox();
+            this.txtAliquotaIRFF = new System.Windows.Forms.TextBox();
+            this.txtSalFamilia = new System.Windows.Forms.TextBox();
+            this.txtSalLiquido = new System.Windows.Forms.TextBox();
+            this.grpbxSexo = new System.Windows.Forms.GroupBox();
+            this.rdbtnFem = new System.Windows.Forms.RadioButton();
+            this.rdbtnMasculino = new System.Windows.Forms.RadioButton();
+            this.grpbxEstadoCivil = new System.Windows.Forms.GroupBox();
+            this.chkCasado = new System.Windows.Forms.CheckBox();
+            this.lblDescontoINSS = new System.Windows.Forms.Label();
+            this.lblDescontoIRPF = new System.Windows.Forms.Label();
+            this.txtDescontoINSS = new System.Windows.Forms.TextBox();
+            this.txtDescontoIRPF = new System.Windows.Forms.TextBox();
+            this.grpbxSexo.SuspendLayout();
+            this.grpbxEstadoCivil.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNomeFunc
+            // 
+            this.lblNomeFunc.AutoSize = true;
+            this.lblNomeFunc.Location = new System.Drawing.Point(13, 27);
+            this.lblNomeFunc.Name = "lblNomeFunc";
+            this.lblNomeFunc.Size = new System.Drawing.Size(108, 13);
+            this.lblNomeFunc.TabIndex = 0;
+            this.lblNomeFunc.Text = "Nome do Funcionario";
+            // 
+            // lblSalBruto
+            // 
+            this.lblSalBruto.AutoSize = true;
+            this.lblSalBruto.Location = new System.Drawing.Point(13, 57);
+            this.lblSalBruto.Name = "lblSalBruto";
+            this.lblSalBruto.Size = new System.Drawing.Size(67, 13);
+            this.lblSalBruto.TabIndex = 1;
+            this.lblSalBruto.Text = "Salario Bruto";
+            // 
+            // lblNumFilhos
+            // 
+            this.lblNumFilhos.AutoSize = true;
+            this.lblNumFilhos.Location = new System.Drawing.Point(13, 93);
+            this.lblNumFilhos.Name = "lblNumFilhos";
+            this.lblNumFilhos.Size = new System.Drawing.Size(89, 13);
+            this.lblNumFilhos.TabIndex = 2;
+            this.lblNumFilhos.Text = "Numero de Filhos";
+            // 
+            // lblDados
+            // 
+            this.lblDados.AutoSize = true;
+            this.lblDados.Location = new System.Drawing.Point(26, 205);
+            this.lblDados.Name = "lblDados";
+            this.lblDados.Size = new System.Drawing.Size(41, 13);
+            this.lblDados.TabIndex = 3;
+            this.lblDados.Text = "Dados:";
+            // 
+            // lblAliquotaINSS
+            // 
+            this.lblAliquotaINSS.AutoSize = true;
+            this.lblAliquotaINSS.Location = new System.Drawing.Point(16, 261);
+            this.lblAliquotaINSS.Name = "lblAliquotaINSS";
+            this.lblAliquotaINSS.Size = new System.Drawing.Size(73, 13);
+            this.lblAliquotaINSS.TabIndex = 4;
+            this.lblAliquotaINSS.Text = "Aliquota INSS";
+            // 
+            // lblAliquotaIRFF
+            // 
+            this.lblAliquotaIRFF.AutoSize = true;
+            this.lblAliquotaIRFF.Location = new System.Drawing.Point(13, 290);
+            this.lblAliquotaIRFF.Name = "lblAliquotaIRFF";
+            this.lblAliquotaIRFF.Size = new System.Drawing.Size(72, 13);
+            this.lblAliquotaIRFF.TabIndex = 5;
+            this.lblAliquotaIRFF.Text = "Aliquota IRPF";
+            // 
+            // lblSalFamilia
+            // 
+            this.lblSalFamilia.AutoSize = true;
+            this.lblSalFamilia.Location = new System.Drawing.Point(16, 330);
+            this.lblSalFamilia.Name = "lblSalFamilia";
+            this.lblSalFamilia.Size = new System.Drawing.Size(74, 13);
+            this.lblSalFamilia.TabIndex = 6;
+            this.lblSalFamilia.Text = "Salario Familia";
+            // 
+            // lblSalLiquido
+            // 
+            this.lblSalLiquido.AutoSize = true;
+            this.lblSalLiquido.Location = new System.Drawing.Point(13, 365);
+            this.lblSalLiquido.Name = "lblSalLiquido";
+            this.lblSalLiquido.Size = new System.Drawing.Size(76, 13);
+            this.lblSalLiquido.TabIndex = 7;
+            this.lblSalLiquido.Text = "Salario Liquido";
+            // 
+            // txtNomeFunc
+            // 
+            this.txtNomeFunc.Location = new System.Drawing.Point(165, 19);
+            this.txtNomeFunc.Name = "txtNomeFunc";
+            this.txtNomeFunc.Size = new System.Drawing.Size(100, 20);
+            this.txtNomeFunc.TabIndex = 8;
+            // 
+            // txtSalBruto
+            // 
+            this.txtSalBruto.Location = new System.Drawing.Point(165, 57);
+            this.txtSalBruto.Name = "txtSalBruto";
+            this.txtSalBruto.Size = new System.Drawing.Size(100, 20);
+            this.txtSalBruto.TabIndex = 9;
+            // 
+            // txtNumFilhos
+            // 
+            this.txtNumFilhos.Location = new System.Drawing.Point(165, 93);
+            this.txtNumFilhos.Name = "txtNumFilhos";
+            this.txtNumFilhos.Size = new System.Drawing.Size(100, 20);
+            this.txtNumFilhos.TabIndex = 10;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(189, 161);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(127, 23);
+            this.btnVerificar.TabIndex = 11;
+            this.btnVerificar.Text = "Verifica Desconto";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // txtAliquotaINSS
+            // 
+            this.txtAliquotaINSS.Location = new System.Drawing.Point(165, 253);
+            this.txtAliquotaINSS.Name = "txtAliquotaINSS";
+            this.txtAliquotaINSS.Size = new System.Drawing.Size(100, 20);
+            this.txtAliquotaINSS.TabIndex = 12;
+            // 
+            // txtAliquotaIRFF
+            // 
+            this.txtAliquotaIRFF.Location = new System.Drawing.Point(165, 290);
+            this.txtAliquotaIRFF.Name = "txtAliquotaIRFF";
+            this.txtAliquotaIRFF.Size = new System.Drawing.Size(100, 20);
+            this.txtAliquotaIRFF.TabIndex = 13;
+            // 
+            // txtSalFamilia
+            // 
+            this.txtSalFamilia.Location = new System.Drawing.Point(165, 330);
+            this.txtSalFamilia.Name = "txtSalFamilia";
+            this.txtSalFamilia.Size = new System.Drawing.Size(100, 20);
+            this.txtSalFamilia.TabIndex = 14;
+            // 
+            // txtSalLiquido
+            // 
+            this.txtSalLiquido.Location = new System.Drawing.Point(165, 365);
+            this.txtSalLiquido.Name = "txtSalLiquido";
+            this.txtSalLiquido.Size = new System.Drawing.Size(100, 20);
+            this.txtSalLiquido.TabIndex = 15;
+            // 
+            // grpbxSexo
+            // 
+            this.grpbxSexo.Controls.Add(this.rdbtnMasculino);
+            this.grpbxSexo.Controls.Add(this.rdbtnFem);
+            this.grpbxSexo.Location = new System.Drawing.Point(507, 27);
+            this.grpbxSexo.Name = "grpbxSexo";
+            this.grpbxSexo.Size = new System.Drawing.Size(200, 100);
+            this.grpbxSexo.TabIndex = 16;
+            this.grpbxSexo.TabStop = false;
+            this.grpbxSexo.Text = "Sexo";
+            // 
+            // rdbtnFem
+            // 
+            this.rdbtnFem.AutoSize = true;
+            this.rdbtnFem.Location = new System.Drawing.Point(7, 25);
+            this.rdbtnFem.Name = "rdbtnFem";
+            this.rdbtnFem.Size = new System.Drawing.Size(31, 17);
+            this.rdbtnFem.TabIndex = 0;
+            this.rdbtnFem.TabStop = true;
+            this.rdbtnFem.Text = "F";
+            this.rdbtnFem.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnMasculino
+            // 
+            this.rdbtnMasculino.AutoSize = true;
+            this.rdbtnMasculino.Location = new System.Drawing.Point(7, 61);
+            this.rdbtnMasculino.Name = "rdbtnMasculino";
+            this.rdbtnMasculino.Size = new System.Drawing.Size(34, 17);
+            this.rdbtnMasculino.TabIndex = 1;
+            this.rdbtnMasculino.TabStop = true;
+            this.rdbtnMasculino.Text = "M";
+            this.rdbtnMasculino.UseVisualStyleBackColor = true;
+            // 
+            // grpbxEstadoCivil
+            // 
+            this.grpbxEstadoCivil.Controls.Add(this.chkCasado);
+            this.grpbxEstadoCivil.Location = new System.Drawing.Point(507, 144);
+            this.grpbxEstadoCivil.Name = "grpbxEstadoCivil";
+            this.grpbxEstadoCivil.Size = new System.Drawing.Size(200, 87);
+            this.grpbxEstadoCivil.TabIndex = 17;
+            this.grpbxEstadoCivil.TabStop = false;
+            // 
+            // chkCasado
+            // 
+            this.chkCasado.AutoSize = true;
+            this.chkCasado.Location = new System.Drawing.Point(7, 40);
+            this.chkCasado.Name = "chkCasado";
+            this.chkCasado.Size = new System.Drawing.Size(62, 17);
+            this.chkCasado.TabIndex = 0;
+            this.chkCasado.Text = "Casado";
+            this.chkCasado.UseVisualStyleBackColor = true;
+            // 
+            // lblDescontoINSS
+            // 
+            this.lblDescontoINSS.AutoSize = true;
+            this.lblDescontoINSS.Location = new System.Drawing.Point(380, 260);
+            this.lblDescontoINSS.Name = "lblDescontoINSS";
+            this.lblDescontoINSS.Size = new System.Drawing.Size(81, 13);
+            this.lblDescontoINSS.TabIndex = 18;
+            this.lblDescontoINSS.Text = "Desconto INSS";
+            // 
+            // lblDescontoIRPF
+            // 
+            this.lblDescontoIRPF.AutoSize = true;
+            this.lblDescontoIRPF.Location = new System.Drawing.Point(380, 297);
+            this.lblDescontoIRPF.Name = "lblDescontoIRPF";
+            this.lblDescontoIRPF.Size = new System.Drawing.Size(80, 13);
+            this.lblDescontoIRPF.TabIndex = 19;
+            this.lblDescontoIRPF.Text = "Desconto IRPF";
+            // 
+            // txtDescontoINSS
+            // 
+            this.txtDescontoINSS.Location = new System.Drawing.Point(498, 261);
+            this.txtDescontoINSS.Name = "txtDescontoINSS";
+            this.txtDescontoINSS.Size = new System.Drawing.Size(100, 20);
+            this.txtDescontoINSS.TabIndex = 20;
+            // 
+            // txtDescontoIRPF
+            // 
+            this.txtDescontoIRPF.Location = new System.Drawing.Point(498, 297);
+            this.txtDescontoIRPF.Name = "txtDescontoIRPF";
+            this.txtDescontoIRPF.Size = new System.Drawing.Size(100, 20);
+            this.txtDescontoIRPF.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDescontoIRPF);
+            this.Controls.Add(this.txtDescontoINSS);
+            this.Controls.Add(this.lblDescontoIRPF);
+            this.Controls.Add(this.lblDescontoINSS);
+            this.Controls.Add(this.grpbxEstadoCivil);
+            this.Controls.Add(this.grpbxSexo);
+            this.Controls.Add(this.txtSalLiquido);
+            this.Controls.Add(this.txtSalFamilia);
+            this.Controls.Add(this.txtAliquotaIRFF);
+            this.Controls.Add(this.txtAliquotaINSS);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtNumFilhos);
+            this.Controls.Add(this.txtSalBruto);
+            this.Controls.Add(this.txtNomeFunc);
+            this.Controls.Add(this.lblSalLiquido);
+            this.Controls.Add(this.lblSalFamilia);
+            this.Controls.Add(this.lblAliquotaIRFF);
+            this.Controls.Add(this.lblAliquotaINSS);
+            this.Controls.Add(this.lblDados);
+            this.Controls.Add(this.lblNumFilhos);
+            this.Controls.Add(this.lblSalBruto);
+            this.Controls.Add(this.lblNomeFunc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpbxSexo.ResumeLayout(false);
+            this.grpbxSexo.PerformLayout();
+            this.grpbxEstadoCivil.ResumeLayout(false);
+            this.grpbxEstadoCivil.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNomeFunc;
+        private System.Windows.Forms.Label lblSalBruto;
+        private System.Windows.Forms.Label lblNumFilhos;
+        private System.Windows.Forms.Label lblDados;
+        private System.Windows.Forms.Label lblAliquotaINSS;
+        private System.Windows.Forms.Label lblAliquotaIRFF;
+        private System.Windows.Forms.Label lblSalFamilia;
+        private System.Windows.Forms.Label lblSalLiquido;
+        private System.Windows.Forms.TextBox txtNomeFunc;
+        private System.Windows.Forms.TextBox txtSalBruto;
+        private System.Windows.Forms.TextBox txtNumFilhos;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txtAliquotaINSS;
+        private System.Windows.Forms.TextBox txtAliquotaIRFF;
+        private System.Windows.Forms.TextBox txtSalFamilia;
+        private System.Windows.Forms.TextBox txtSalLiquido;
+        private System.Windows.Forms.GroupBox grpbxSexo;
+        private System.Windows.Forms.RadioButton rdbtnMasculino;
+        private System.Windows.Forms.RadioButton rdbtnFem;
+        private System.Windows.Forms.GroupBox grpbxEstadoCivil;
+        private System.Windows.Forms.CheckBox chkCasado;
+        private System.Windows.Forms.Label lblDescontoINSS;
+        private System.Windows.Forms.Label lblDescontoIRPF;
+        private System.Windows.Forms.TextBox txtDescontoINSS;
+        private System.Windows.Forms.TextBox txtDescontoIRPF;
+    }
+}
+
---
+++ Atividade_4/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double verify1, verify2, aliquoINSS, aliquoIRFF, salFamilia, salLiq;
+
+
+
+            if (string.IsNullOrWhiteSpace(txtNomeFunc.Text))
+                MessageBox.Show("Nome esta em  branco");
+            else if (string.IsNullOrWhiteSpace(txtNumFilhos.Text))
+                MessageBox.Show("Não foi digitado o  número de filhos");
+            else
+                if (double.TryParse(txtSalBruto.Text, out verify1) && double.TryParse(txtNumFilhos.Text, out verify2))
+            {
+                if (rdbtnFem.Checked == true || rdbtnMasculino.Checked == true)
+                {
+                    string nome;
+                    nome = txtNomeFunc.Text;
+
+
+                    if (rdbtnFem.Checked == true && chkCasado.Checked == true)
+                        lblDados.Text = string.Format("Os descontos do salário da Sra.{0}\nque é casada\ne tem {1} filho(s) são:", nome, verify2);
+                    if (rdbtnFem.Checked == true && chkCasado.Checked == false)
+                        lblDados.Text = string.Format("Os descontos do salário da Sra.{0}\nque é solteira\ne tem {1} filho(s) são:", nome, verify2);
+                    if (rdbtnFem.Checked == false && chkCasado.Checked == false)
+                        lblDados.Text = string.Format("Os descontos do salário da Sr.{0}\nque é solteiro\ne tem {1} filho(s) são:", nome, verify2);
+                    if (rdbtnFem.Checked == false && chkCasado.Checked == true)
+                        lblDados.Text = string.Format("Os descontos do salário da Sr.{0}\nque é casado\ne tem {1} filho(s) são:", nome, verify2);
+
+
+
+                    //Aliquota INSS
+                    if (verify1 <= 800.47)
+                    {
+                        aliquoINSS = verify1 * (7.65 / 100);
+                        txtAliquotaINSS.Text = "7.65%";
+                    }
+                    else if (verify1 <= 1050)
+                    {
+                        aliquoINSS = verify1 * (8.65 / 100);
+                        txtAliquotaINSS.Text = "8.65 %";
+                    }
+                    else if (verify1 <= 1400.77)
+                    {
+                        aliquoINSS = verify1 * (9 / 100);
+                        txtAliquotaINSS.Text = "9.00 %";
+                    }
+                    else if (verify1 <= 2801.56)
+                    {
+                        aliquoINSS = verify1 * (11 / 100);
+                        txtAliquotaINSS.Text = "11 %";
+                    }
+                    else
+                    {
+                        aliquoINSS = 308.17;
+                        txtAliquotaINSS.Text = "Teto";
+                    }
+
+                    txtDescontoINSS.Text = aliquoINSS.ToString("N2");
+
+
+                    // Aliquota IRFF
+                    if (verify1 <= 1257.12)
+                    {
+                        aliquoIRFF = 0;
+                        txtAliquotaIRFF.Text = "0";
+                    }
+                    else if (verify1 <= 2512.08)
+                    {
+                        aliquoIRFF = verify1 * (15 / 100);
+                        txtAliquotaIRFF.Text = "15%";
+                    }
+                    else
+                    {
+                        aliquoIRFF = verify1 * (27.5 / 100);
+                        txtAliquotaIRFF.Text = "27.5%";
+                    }
+
+                    txtDescontoIRPF.Text = aliquoIRFF.ToString("N2");
+
+                    // Salario familia
+                    if (verify1 <= 435.52)
+                        salFamilia = verify2 * 22.33;
+                    else if (verify1 <= 654.61)
+                        salFamilia = verify2 * 15.74;
+                    else
+                        salFamilia = 0;
+
+                    txtSalFamilia.Text = salFamilia.ToString("N2");
+
+                    // Calculo Salário Liquido
+
+                    salLiq = verify1 - aliquoINSS - aliquoIRFF + salFamilia;
+
+                    txtSalLiquido.Text = salLiq.ToString("N2");
+
+
+
+
+                }
+
+                else
+                    MessageBox.Show("Sexo não selecionado");
+            }
+
+            else
+                MessageBox.Show("Dados Inválidos");
+        }
+    }
+}
