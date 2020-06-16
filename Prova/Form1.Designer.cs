1c575ac1e358f85637e42d5c8320df0d02e094c2
PauloCervencove <62033278+PauloCervencove@users.noreply.github.com>
2020-06-16 11:06:18 -0300

    Add files via upload

---
+++ Prova/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace P0030481921006
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstVendas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(260, 159);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(102, 85);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lstVendas
+            // 
+            this.lstVendas.FormattingEnabled = true;
+            this.lstVendas.Location = new System.Drawing.Point(448, 16);
+            this.lstVendas.Name = "lstVendas";
+            this.lstVendas.Size = new System.Drawing.Size(310, 368);
+            this.lstVendas.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstVendas);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstVendas;
+    }
+}
+
